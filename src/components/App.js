0c493fb3f0c36f44b3f0f23644825c025f0c09a9
Brendan McQuade <bmcquade@demacmedia.com>
2017-03-06 22:58:41 -0500

    Remove demo app and keep sensible starting point

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,20 @@
-import React, { PropTypes } from 'react';
-import { Link, IndexLink } from 'react-router';
+import React, { Component, PropTypes } from 'react';
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
 // component at the top-level.
-class App extends React.Component {
+class App extends Component {
+  static propTypes = {
+    children: PropTypes.element
+  }
+
   render() {
     return (
       <div>
-        <IndexLink to="/">Home</IndexLink>
-        {' | '}
-        <Link to="/fuel-savings">Demo App</Link>
-        {' | '}
-        <Link to="/about">About</Link>
-        <br/>
         {this.props.children}
       </div>
     );
   }
 }
-
-App.propTypes = {
-  children: PropTypes.element
-};
 
 export default App;
